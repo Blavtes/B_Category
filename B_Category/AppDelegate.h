5b4dd5b1a82124c41bb96d30460463c68cfcd4bc
Blavtes <Blavtes@yangyong.local>
2017-11-03 11:20:22 +0800

    Initial Commit

---
+++ B_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  B_Category
+//
+//  Created by Blavtes on 2017/11/3.
+//  Copyright © 2017年 Blavtes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
