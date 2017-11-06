52fd27ec7db7ca1d2ad0301971f6db36179b9b48
Blavtes <Blavtes@yangyong.local>
2017-11-06 10:22:54 +0800

    init

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,17 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by Blavtes on 2017/11/3.
+//  Copyright © 2017年 Blavtes. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+#import <UIKit/UIKit.h>
+
+@interface CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+
+@end
---
+++ upload.sh
@@ -0,0 +1,1 @@
+pod repo push RepoRepository B_Category.podspec --verbose --allow-warnings
