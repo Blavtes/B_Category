dcac274e95ebc39e7cb94a2d934c1f2c1263c700
Blavtes <Blavtes@yangyong.local>
2017-11-06 18:08:46 +0800

    init

--- B_Category.podspec
+++ B_Category.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "B_Category"
-  s.version      = "0.6"
+  s.version      = "0.7"
   s.summary      = "B_Category."
 
   # This description is used to generate tags and improve search results.
--- B_Category/ViewController.h
+++ B_Category/ViewController.h
@@ -2,11 +2,6 @@
 //  ViewController.h
 //  B_Category
 //
-<<<<<<< HEAD
-//  Created by Blavtes on 2017/11/6.
-=======
-//  Created by Blavtes on 2017/11/3.
->>>>>>> 800371389cfd8bc4e9773e278551153d90114882
 //  Copyright © 2017年 Blavtes. All rights reserved.
 //
 
