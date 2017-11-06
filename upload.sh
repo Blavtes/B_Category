2c90d3a8b7b0df2fc75934ce89749395810230f0
Blavtes <Blavtes@yangyong.local>
2017-11-06 15:55:19 +0800

    init

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
+
 pod repo push MyRepo B_Category.podspec --verbose --allow-warnings
-=======
-pod repo push RepoRepository B_Category.podspec --verbose --allow-warnings
->>>>>>> 800371389cfd8bc4e9773e278551153d90114882
+
+
