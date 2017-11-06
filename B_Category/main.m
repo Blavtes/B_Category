d58b4ff248e6ad62398e3b8fae0db989040eada3
Blavtes <Blavtes@yangyong.local>
2017-11-06 18:08:46 +0800

    init

--- B_Category/main.m
+++ B_Category/main.m
@@ -2,11 +2,7 @@
 //  main.m
 //  B_Category
 //
-<<<<<<< HEAD
 //  Created by Blavtes on 2017/11/6.
-=======
-//  Created by Blavtes on 2017/11/3.
->>>>>>> 800371389cfd8bc4e9773e278551153d90114882
 //  Copyright © 2017年 Blavtes. All rights reserved.
 //
 
